b32cf7697e367ae9f523400a328d923c1d008e09
seregeu <vetrser@gmail.com>
2021-09-12 14:22:08 +0300

    add menu and fragments

--- app/src/main/java/com/example/oii_project/MainActivity.kt
+++ app/src/main/java/com/example/oii_project/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.oii_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.setupWithNavController
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.fragment_container)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setupWithNavController(navController)
     }
 }
