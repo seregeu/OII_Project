9bd2063ca951eb534dff2f8bb406c9053dc6141c
seregeu <vetrser@gmail.com>
2021-09-14 23:46:44 +0300

    add transitions

--- app/src/main/java/com/example/oii_project/fragments/UserDetailFragment.kt
+++ app/src/main/java/com/example/oii_project/fragments/UserDetailFragment.kt
@@ -5,28 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.oii_project.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UserDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UserDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -38,23 +27,18 @@ class UserDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_user_detail, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners(view)
     }
+
+    fun setListeners(view: View){
+        navController = view.findNavController()
+
+        val logoutButton: Button = view.findViewById(R.id.logout_button)
+        logoutButton.setOnClickListener{
+            navController.navigate(R.id.action_userDetailFragment_to_loginFragment)
+        }
+    }
+
 }
